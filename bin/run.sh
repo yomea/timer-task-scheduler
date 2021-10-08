11879c5c187ef4f6f884ed7c0a16eacffec9939a
Wu Zhenhong <wuzh@shinemo.com>
2021-10-08 10:37:52 +0800

    debug

--- bin/run.sh
+++ bin/run.sh
@@ -1,51 +1,35 @@
-#!/bin/sh
-source /etc/profile
-
-if [ -z "$2" ]; then
-        echo $1 with online
-        env=online
-        doWhat=$1
-else
-        env=$1
-        doWhat=$2
-fi
+#!/bin/bash
+# Jar Service Startup Scipt
+# SHM QKT
 
 USER=admin
-SERVICEPATH=/home/admin
 SERVICE=$(cd $(dirname $0); pwd | awk -F '/' '{print $(NF-1)}')
+MEMORY=1024m
+ARGS=''
 
-source $SERVICEPATH/${SERVICE}/bin/args.sh
-
-# SERVICE=可以手动定义，默认取上一级目录名
-LOGS=/data/logs
-mkdir -pv ${LOGS}/${SERVICE}
-
-eval Xms="\$Xms_$env"
-eval Xmx="\$Xmx_$env"
-echo $Xms
-echo $Xmx
 Start() {
-    proc=$(ps -ef | grep ${SERVICEPATH}/${SERVICE}/${SERVICE}.jar | grep -v grep | wc -l)
+    mem=$1
+    if [ -z "$mem" ];then
+        mem=$MEMORY
+    fi
+
+    proc=$(ps -ef | grep /home/admin/${SERVICE}/lib/${SERVICE}.jar | grep -v grep | wc -l)
     if [[ $proc != 0  ]];then
         exit 5
     fi
     if [[ `whoami` == 'root' ]];then
-        exec su admin -c "java -server -Xms$Xms -Xmx$Xmx ${GCLOG}  ${HEAPDUMP} $ARGS -Dlogging.config=${SERVICEPATH}/${SERVICE}/config/logback-spring.xml -Dspring.config.location=${SERVICEPATH}/${SERVICE}/config/application.yml   -jar ${SERVICEPATH}/${SERVICE}/${SERVICE}.jar >> ${SERVICEPATH}/${SERVICE}/${SERVICE}.log 2>&1 &"
+        exec su admin -c "java -server -Xms${mem} -Xmx${mem}  -jar /home/admin/${SERVICE}/lib/${SERVICE}.jar  $ARGS >> /data/logs/${SERVICE}.log 2>&1 &"
     elif [[ `whoami` == 'admin' ]];then
-        exec java -server -Xms$Xms -Xmx$Xmx ${GCLOG}  ${HEAPDUMP}  $ARGS -Dlogging.config=${SERVICEPATH}/${SERVICE}/config/logback-spring.xml -Dspring.config.location=${SERVICEPATH}/${SERVICE}/config/application.yml   -jar ${SERVICEPATH}/${SERVICE}/${SERVICE}.jar  >> ${SERVICEPATH}/${SERVICE}/${SERVICE}.log 2>&1 &
+        exec java -server -Xms${mem} -Xmx${mem}  -jar /home/admin/${SERVICE}/lib/${SERVICE}.jar  $ARGS >> /data/logs/${SERVICE}.log 2>&1 &
     else
         echo "Run with admin user"
         exit 10
     fi
 }
 
+
 Stop() {
-    ps -ef | grep ${SERVICEPATH}/${SERVICE}/${SERVICE}.jar | grep -v grep | awk '{print $2}'| xargs kill
-    sleep 3
-    proc=$(ps -ef | grep ${SERVICEPATH}/${SERVICE}/${SERVICE}.jar | grep -v grep | wc -l)
-    if [[ $proc != 0 ]];then
-        ps -ef | grep ${SERVICEPATH}/${SERVICE}/${SERVICE}.jar | grep -v grep | awk '{print $2}'| xargs kill -9
-    fi
+    /usr/bin/ps -ef | grep ${SERVICE} | grep -v grep | awk '{print $2}'| xargs kill -9
 }
 
 Restart() {
@@ -53,17 +37,15 @@ Restart() {
     Start
 }
 
-case $doWhat in
+
+case $1 in
     start|run)
-        Start
+        Start $2
         ;;
     stop)
         Stop
         ;;
     restart)
         Restart
-        ;;
-    *)
-        Start
         ;;
 esac
--- src/test/java/com/shinemo/task/RedisTest.java
+++ src/test/java/com/shinemo/task/RedisTest.java
@@ -1,75 +1,75 @@
-package com.shinemo.task;
-
-import com.shinemo.Aace.orginfo.model.OrgInfo;
-import com.shinemo.common.tools.redis.RedisLock;
-import com.shinemo.util.GsonUtil;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import javax.annotation.Resource;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * @author zhaoyn
- * @Date 2019/11/15
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, properties = {"spring.profiles.active=daily"})
-public class RedisTest {
-
-    @Autowired
-    private RedisTemplate<String, String> stringRedisTemplate;
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-    @Resource
-    private RedisLock redisLock;
-
-    private ExecutorService pool = Executors.newFixedThreadPool(20);
-
-    @Test
-    public void testRedis() {
-        String key = "flowable-ubpm-zyntest";
-        stringRedisTemplate.opsForValue().set(key, "test");
-        String value = stringRedisTemplate.opsForValue().get(key);
-        System.out.println(value);
-    }
-
-
-    @Test
-    public void testRedisTemplate() {
-        String key = "hello";
-        OrgInfo orgInfo = new OrgInfo();
-        orgInfo.setAddress("杭州市西湖区");
-        orgInfo.setCustomName("张三");
-        redisTemplate.opsForValue().set(key, orgInfo, 1, TimeUnit.MINUTES);
-        OrgInfo value = (OrgInfo) redisTemplate.opsForValue().get(key);
-        System.out.println(GsonUtil.toJson(value));
-    }
-
-    @Test
-    public void testRedisLock() {
-        List<CompletableFuture<Boolean>> futures = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() -> {
-                boolean lock = redisLock.tryLock("zyn_test_hhh", 5);
-                return lock;
-            }, pool);
-            futures.add(future);
-        }
-        futures.forEach(v -> {
-            boolean result = v.join();
-            System.out.println(System.currentTimeMillis() + "----------" + result);
-        });
-    }
-
-
-}
-
+//package com.shinemo.task;
+//
+//import com.shinemo.common.tools.redis.RedisLock;
+//import com.shinemo.util.GsonUtil;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import javax.annotation.Resource;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * @author zhaoyn
+// * @Date 2019/11/15
+// */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class, properties = {"spring.profiles.active=daily"})
+//public class RedisTest {
+//
+//    @Autowired
+//    private RedisTemplate<String, String> stringRedisTemplate;
+//    @Autowired
+//    private RedisTemplate<String, Object> redisTemplate;
+//    @Resource
+//    private RedisLock redisLock;
+//
+//    private ExecutorService pool = Executors.newFixedThreadPool(20);
+//
+//    @Test
+//    public void testRedis() {
+//        String key = "flowable-ubpm-zyntest";
+//        stringRedisTemplate.opsForValue().set(key, "test");
+//        String value = stringRedisTemplate.opsForValue().get(key);
+//        System.out.println(value);
+//    }
+//
+//
+//    @Test
+//    public void testRedisTemplate() {
+//        String key = "hello";
+//        OrgInfo orgInfo = new OrgInfo();
+//        orgInfo.setAddress("杭州市西湖区");
+//        orgInfo.setCustomName("张三");
+//        redisTemplate.opsForValue().set(key, orgInfo, 1, TimeUnit.MINUTES);
+//        OrgInfo value = (OrgInfo) redisTemplate.opsForValue().get(key);
+//        System.out.println(GsonUtil.toJson(value));
+//    }
+//
+//    @Test
+//    public void testRedisLock() {
+//        List<CompletableFuture<Boolean>> futures = new ArrayList<>();
+//        for (int i = 0; i < 50; i++) {
+//            CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() -> {
+//                boolean lock = redisLock.tryLock("zyn_test_hhh", 5);
+//                return lock;
+//            }, pool);
+//            futures.add(future);
+//        }
+//        futures.forEach(v -> {
+//            boolean result = v.join();
+//            System.out.println(System.currentTimeMillis() + "----------" + result);
+//        });
+//    }
+//
+//
+//}
+//
